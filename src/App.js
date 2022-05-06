b7b0eacaa1c334772e01844ff385312f226a028c
Julien ichard WCS <julien.y.richard@gmail.com>
2022-05-06 14:25:41 +0200

    Mise en place de la requete sur le Endpoint Meta Api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import axios from 'axios';
 import './App.css';
 
 function App() {
   const handleClick = () => {
-    console.log('OK')
+    axios.get(`https://api.meta-api.io/api/spells/${process.env.REACT_APP_META_API_SPELL}/runSync?apikey=${process.env.REACT_APP_META_API}`)
+      .then(data => console.log(data))
+      .catch(err => console.error(err))
   }
-  
+
   return (
     <div className="App">
       <h1> Meta Api connexion</h1>
